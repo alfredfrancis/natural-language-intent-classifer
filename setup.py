11efbef023a56bbee2c5127a12dacfd8e27d43e6
Alfred <alfredfranciz@gmail.com>
2016-12-22 23:43:17 +0530

    Removed unwanted datasets

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ Downloading necessary NLTK datasets
 
 nltk.download("stopwords")
 nltk.download("wordnet")
-nltk.download('averaged_perceptron_tagger')
-nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('punkt')
